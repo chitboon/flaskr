ae87203d70cc2fd30e3aa2cefd4b6a07ca24671f
chitboon <chitboon@gmail.com>
2018-11-14 01:16:37 +0800

    Updated

--- app.py
+++ app.py
@@ -1,29 +1,124 @@
 from flask import *
 from persistence import *
+import functools
 
 app = Flask(__name__)
+app.config.from_mapping(
+    SECRET_KEY='dev'
+)
 
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if session['id'] is None:
+            return redirect(url_for('login'))
+        return view(**kwargs)
+    return wrapped_view
+
+@app.route('/init')
+def init():
+    init_db()
+    return 'db initialised'
 
 @app.route('/')
-def hello_world():
-    return 'Hello %s' % 'xx'
-
-@app.route('/create/<name>')
-def create(name):
-    create_user('1', name)
-    return 'created %s' % name
-
-@app.route('/get/<id>')
-def get(id):
-    name = get_user(id)
-    return 'retrieved %s' % name
-
-@app.route('/update/<name>')
-def update(name):
-    user = update_user('1', name)
-    return 'updated %s' % user
+def index():
+    if 'id' in session:
+        posts = get_blogs()
+        return render_template('index.html', posts = posts)
+    else:
+        return render_template('login.html')
 
 
+@app.route('/login',  methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        error = None
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        else:
+            user = get_user(username, password)
+            if user is None:
+                error = 'Wrong username and password'
+            else:
+                session['id'] = user.get_id()
+                session['user_name'] = user.get_username()
+                return redirect(url_for('index'))
+        flash(error)
+    return render_template('login.html')
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        error = None
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        else:
+            create_user(username, password)
+            return redirect(url_for('login'))
+        flash(error)
+    return render_template('register.html')
+
+@app.route('/<string:id>/update', methods=('GET', 'POST'))
+def update(id):
+    post = get_blog(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            post.title = title
+            post.body = body
+            update_blog(post)
+            return redirect(url_for('index'))
+
+    return render_template('update.html', post=post)
+
+@app.route('/<string:id>/delete', methods=('GET', 'POST'))
+def delete(id):
+    delete_blog(id)
+    posts = get_blogs()
+    return render_template('index.html', posts=posts)
+
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            create_blog(session['user_name'], title, body)
+            return redirect(url_for('index'))
+
+    return render_template('create.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run()
+
+
+
--- persistence.py
+++ persistence.py
@@ -1,14 +1,106 @@
 import shelve
+import uuid
+from datetime import date
+# today = str(date.today())
 
-user = shelve.open('user')
+class User:
+    def __init__(self, id):
+        self.__id = id
+        self.__username = ''
+        self.__password = ''
+
+    def get_id(self):
+        return self.__id
+
+    def set_username(self, username):
+        self.__username = username
+
+    def set_password(self, password):
+        self.__password = password
+
+    def get_username(self):
+        return self.__username
+
+    def get_password(self):
+        return self.__password
+
+class Blog:
+    def __init__(self, id):
+        self.id = id
+        self.username = ''
+        self.title = ''
+        self.body = ''
+        self.created = ''
+
+users = shelve.open('user')
+blogs = shelve.open('blog')
+
+def create_blog(username, title, body):
+    id = str(uuid.uuid4())
+    blog = Blog(id)
+    blog.title = title
+    blog.username = username
+    blog.body = body
+    blog.created = str(date.today())
+    blogs[id] = blog
+
+def update_blog(blog):
+    blogs[blog.id] = blog
+
+def delete_blog(id):
+    if id in blogs:
+        del blogs[id]
+
+def get_blogs():
+    klist = list(blogs.keys())
+    x = []
+    for i in klist:
+        x.append(blogs[i])
+    return x
+
+def get_blog(id):
+    if id in blogs:
+        return blogs[id]
+
+def create_user(username, password):
+    id = str(uuid.uuid4())
+    user = User(id)
+    user.set_username(username)
+    user.set_password(password)
+    users[id] = user
+
+def get_user(username, password):
+    klist = list(users.keys())
+    for key in klist:
+        user = users[key]
+        print(user.get_username(), username, user.get_password(), password)
+        if user.get_username() == username and user.get_password() == password:
+            return user
+    return None
+
+def update_user(id, user):
+    users[id] = user
+    return users[id]
+
+def clear_user():
+    klist = list(users.keys())
+    for key in klist:
+        del users[key]
+
+def clear_blog():
+    klist = list(blogs.keys())
+    for key in klist:
+        del blogs[key]
+
+def add_user(user):
+    users[user.get_id()] = user
+
+def init_db():
+    clear_user()
+    clear_blog()
+    for i in range(5):
+        create_user('user'+str(i), 'pass'+str(i))
+        create_blog('user'+str(i), 'title'+str(i), 'body'+str(i))
 
 
-def create_user(id, name):
-    user[id] = name
 
-def get_user(id):
-    return user[id]
-
-def update_user(id, name):
-    user[id] = name
-    return user[id]
